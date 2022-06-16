09f5a2b81f3120bdc352e93663a42a88ed146fe2
miyukitamura <miyuki3672@icloud.com>
2022-06-16 11:21:19 +0000

    Finish page-layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'static_pages/top'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#top' #rootにするとURL/へアクセスした時にトップページが表示されるようになる
+  get '/signup', to: 'user#new'
 end
